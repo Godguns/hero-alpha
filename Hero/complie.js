61f6e7fb2d266964c64c3ed914dd448b13d7e1ca
dongjinlang <dongjinlang@hikvision.com.cn>
2021-09-18 16:43:41 +0800

    编译功能

--- Hero/complie.js
+++ Hero/complie.js
@@ -1,13 +1,19 @@
 var fs = require('fs');
 var path = require('path');
+const { Script } = require('vm');
 let newTemplate = null;
 let temPlateScript = null;
-function templateCreate() {
+(function templateCreate() {
     newTemplate = fs.readFileSync(path.join(__dirname, '../src/App.Hero')).toString();
-    
-    //newTemplate = `<!DOCTYPE html><html lang="en"><body><div id="app">${data}</div></body><script src="./hero.js"></script></html>`.trim();
-    temPlateScript = newTemplate.match(/(?<=<HeroScript>)[\d\D]*(?=<\/HeroScript)/m);
-}
-templateCreate();
-module.exports = {newTemplate,temPlateScript};
+    temPlateScript =`<script>${newTemplate.match(/(?<=<HeroScript>)[\d\D]*(?=<\/HeroScript)/m)}</script>\n</body>`;
+    newTemplate =`<div>${ newTemplate.match(/(?<=<Fragment>)[\d\D]*(?=<\/Fragment>)/m)}</div>`;
+})();
+fs.readFile(path.join(__dirname,'../src/index.html'),(err,data)=>{
+    fs.writeFileSync(path.join(__dirname,'../src/index.html'),data.toString().replace(/<Hero>[\d\D]*<\/Hero>/m,newTemplate))
+    fs.readFile(path.join(__dirname,'../src/index.html'),(err,ret)=>{
+        fs.writeFileSync(path.join(__dirname,'../src/index.html'),ret.toString().replace(/(<HeroScript>)[\d\D]*(<\/HeroScript>)/m,temPlateScript))
+    })
+})
+
+
 
